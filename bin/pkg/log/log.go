945f34060bd4e2d879ec006b78f8602eebf64836
Farid Tri Wicaksono <farid.wicak@gmail.com>
2024-11-26 09:55:20 +0700

    fix: adjust log

--- bin/pkg/log/log.go
+++ bin/pkg/log/log.go
@@ -1,7 +1,6 @@
 package log
 
 import (
-	"encoding/json"
 	"fmt"
 	"runtime"
 
@@ -39,32 +38,32 @@ func GetLogger() Log {
 	return logger
 }
 
-type logstashPayload struct {
-	ServiceName interface{} `json:"serviceName"`
-	Context     string      `json:"context"`
-	Scope       string      `json:"scope"`
-	Message     string      `json:"message"`
-	Meta        string      `json:"meta"`
-	Level       string      `json:"level"`
-	Label       string      `json:"label"`
-}
+// type logstashPayload struct {
+// 	ServiceName interface{} `json:"serviceName"`
+// 	Context     string      `json:"context"`
+// 	Scope       string      `json:"scope"`
+// 	Message     string      `json:"message"`
+// 	Meta        string      `json:"meta"`
+// 	Level       string      `json:"level"`
+// 	Label       string      `json:"label"`
+// }
 
 func (l Log) Info(context, message, scope, meta string) {
 	if l.logLevel <= 1 {
 		_, file, line, _ := runtime.Caller(1)
 		msg := fmt.Sprintf("[INFO] Service: %s - Context: %s - Message: %s - Scope: %s - Meta: %s - At: %s:%d", "MESSAGING_SERVICE", context, message, scope, meta, file, line)
 		println(msg)
-		json.Unmarshal([]byte(`{"service": "MESSAGING_SERVICE"}`), &serviceName)
-		ls, _ := json.Marshal(logstashPayload{
-			ServiceName: serviceName,
-			Level:       "info",
-			Meta:        meta,
-			Message:     message,
-			Context:     context,
-			Label:       l.appName,
-			Scope:       scope,
-		})
-		go l.sendLogstash(string(ls))
+		// json.Unmarshal([]byte(`{"service": "MESSAGING_SERVICE"}`), &serviceName)
+		// ls, _ := json.Marshal(logstashPayload{
+		// 	ServiceName: serviceName,
+		// 	Level:       "info",
+		// 	Meta:        meta,
+		// 	Message:     message,
+		// 	Context:     context,
+		// 	Label:       l.appName,
+		// 	Scope:       scope,
+		// })
+		// go l.sendLogstash(string(ls))
 	}
 }
 
@@ -75,17 +74,17 @@ func (l Log) Error(context, message, scope, meta string) {
 		msg := fmt.Sprintf("[ERROR] Context: %s - Message: %s - Scope: %s - Meta: %s - At Level1: %s:%d - At Level2: %s:%d", context, message, scope, meta, file, line, file2, line2)
 		println(msg)
 
-		json.Unmarshal([]byte(`{"service": "MESSAGING_SERVICE"}`), &serviceName)
-		ls, _ := json.Marshal(logstashPayload{
-			ServiceName: serviceName,
-			Level:       "error",
-			Meta:        meta,
-			Message:     message,
-			Context:     context,
-			Label:       l.appName,
-			Scope:       scope,
-		})
-		go l.sendLogstash(string(ls))
+		// json.Unmarshal([]byte(`{"service": "MESSAGING_SERVICE"}`), &serviceName)
+		// ls, _ := json.Marshal(logstashPayload{
+		// 	ServiceName: serviceName,
+		// 	Level:       "error",
+		// 	Meta:        meta,
+		// 	Message:     message,
+		// 	Context:     context,
+		// 	Label:       l.appName,
+		// 	Scope:       scope,
+		// })
+		// go l.sendLogstash(string(ls))
 	}
 }
 
@@ -95,23 +94,23 @@ func (l Log) Slow(context, message, scope, meta string) {
 		msg := fmt.Sprintf("[SLOW] Context: %s - Message: %s - Scope: %s - Meta: %s - At: %s:%d", context, message, scope, meta, file, line)
 		println(msg)
 
-		json.Unmarshal([]byte(`{"service": "MESSAGING_SERVICE"}`), &serviceName)
-		ls, _ := json.Marshal(logstashPayload{
-			ServiceName: serviceName,
-			Level:       "slow",
-			Meta:        meta,
-			Message:     message,
-			Context:     context,
-			Label:       l.appName,
-			Scope:       scope,
-		})
-		go l.sendLogstash(string(ls))
+		// json.Unmarshal([]byte(`{"service": "MESSAGING_SERVICE"}`), &serviceName)
+		// ls, _ := json.Marshal(logstashPayload{
+		// 	ServiceName: serviceName,
+		// 	Level:       "slow",
+		// 	Meta:        meta,
+		// 	Message:     message,
+		// 	Context:     context,
+		// 	Label:       l.appName,
+		// 	Scope:       scope,
+		// })
+		// go l.sendLogstash(string(ls))
 	}
 }
 
-func (l Log) sendLogstash(msg string) {
+// func (l Log) sendLogstash(msg string) {
 
-	if err := logstsh.Writeln(string(msg)); err != nil {
-		println(err.Error())
-	}
-}
+// 	if err := logstsh.Writeln(string(msg)); err != nil {
+// 		println(err.Error())
+// 	}
+// }
